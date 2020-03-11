004bde8845df5b2e8fd9150ad5ec39f21514a989
lsc <lsc@localhost>
2020-03-11 11:23:34 +0900

    connection 수정

--- src/test/java/com/db/test/RabbitMQ_NewTaskWorker.java
+++ src/test/java/com/db/test/RabbitMQ_NewTaskWorker.java
@@ -10,8 +10,8 @@ public class RabbitMQ_NewTaskWorker {
 	private static final String TASK_QUEUE_NAME = "task_queue";
 
 	public static void main(String[] argv) throws Exception {
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		ConnectionFactory factory = rabbitmqConnectionFactory();
+
 		final Connection connection = factory.newConnection();
 		final Channel channel = connection.createChannel();
 
